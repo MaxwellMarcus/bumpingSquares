13a82ba92573a1a339a575bbfc512be16a745ef9
bigmaxattack37 <bigmaxmarcus@gmail.com>
2019-01-13 11:21:32 -0600

    I am pretty much finnished the only other thing I am going to add is a start screen.

--- pong.py
+++ pong.py
@@ -11,10 +11,14 @@ canvas.pack()
 class Ball:
     def __init__(self):
         self.acceleration = [0,0]#this probably won't be used much but it is good to add one in any way
-        self.velocity = [random.randint(2,6),random.randint(2,6)]# this will keep track of the speed in x and y
+        self.velocity = [random.randint(-6,6)/50,random.randint(-6,6)/50]# this will keep track of the speed in x and y
+        while self.velocity[1] == 0 or self.velocity[0] == 0:
+            self.velocity = [random.randint(-6,6)/50,random.randint(-6,6)/50]
         self.position = [500,250]# this will keep track of the x and y position
         self.ball = canvas.create_rectangle(0,0,0,0)
-
+        self.paddle1 = Paddle(50,'arrows')
+        self.paddle2 = Paddle(950,'ws')
+        self.pause = True
     def updateVelocity(self):
         self.velocity[0] += self.acceleration[0]
         self.velocity[1] += self.acceleration[1]
@@ -23,11 +27,19 @@ class Ball:
         self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
 
-        if self.position[0]>1000 or self.position[0]<0:
-            self.velocity[0] = -self.velocity[0]
+        if (abs(self.position[0]-self.paddle1.position[0])<6 and self.position[1] < self.paddle1.position[1]+50 and self.position[1] > self.paddle1.position[1]-50) or (abs(self.position[0]-self.paddle2.position[0])<6 and self.position[1] < self.paddle2.position[1]+50 and self.position[1] > self.paddle2.position[1]-50):
+            self.velocity[0] = -self.velocity[0]*2
         if self.position[1]>500 or self.position[1]<0:
             self.velocity[1] = -self.velocity[1]
 
+        if self.position[0] < 0 or self.position[0] > 1000:
+            self.pause = True
+            self.position = [500,250]
+            self.paddle1.position[1] = 250
+            self.paddle2.position[1] = 250
+            self.velocity = [random.randint(-6,6)/50,random.randint(-6,6)/50]
+            while self.velocity[1] == 0 or self.velocity[0] == 0:
+                self.velocity = [random.randint(-6,6)/50,random.randint(-6,6)/50]
     def render(self):
         canvas.delete(self.ball)
         self.ball = canvas.create_rectangle(self.position[0]-5,self.position[1]-5,self.position[0]+5,self.position[1]+5,fill='white')
@@ -43,19 +55,39 @@ class Paddle:
         self.position = [x,250]
         self.paddle = canvas.create_rectangle(self.position[0],self.position[1]+50,self.position[0]-5,self.position[1]-50)
         if inputs == 'arrows':
-            root.bind('Up',self.movePaddleUp)
-            root.bind('Down',self.movePaddleDown)
+            root.bind('<Up>',self.movePaddleUp)
+            root.bind('<Down>',self.movePaddleDown)
+        if inputs == 'ws':
+            root.bind('w',self.movePaddleUp)
+            root.bind('s',self.movePaddleDown)
     def movePaddleUp(self,event):
-        self.position[1] -= 2
+        self.position[1] -= 5
     def movePaddleDown(self,event):
-        self.position[1] += 2
+        self.position[1] += 5
     def render(self):
         canvas.delete(self.paddle)
         self.paddle = canvas.create_rectangle(self.position[0],self.position[1]+50,self.position[0]-5,self.position[1]-50,fill='white')
 
 ball = Ball()
-paddle = Paddle(50,'arrows')
-while True:
-    ball.update()
-    paddle.render()
+
+def keyPress(event):
+    if event.keysym == 'Escape':
+        global game
+        game = False
+    if event.keysym == 'space':
+        ball.update()
+        ball.paddle1.render()
+        ball.paddle2.render()
+        ball.pause = not ball.pause
+
+root.bind('<KeyPress>',keyPress)
+game = True
+ball.update()
+ball.paddle1.render()
+ball.paddle2.render()
+while game:
+    if not ball.pause:
+        ball.update()
+        ball.paddle1.render()
+        ball.paddle2.render()
     root.update()
